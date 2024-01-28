4b447d0b8d2836492e8d1d1b08ef4453e6f18e78
JakeKitzmann <97077033+JakeKitzmann@users.noreply.github.com>
2024-01-28 17:39:34 -0600

    Homework One Complete

---
+++ HomeworkOne/src/BinaryHeap.java
@@ -0,0 +1,73 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class BinaryHeap {
+
+    ArrayList<Integer> heap;
+
+    // constructor -- creates binary heap with input Arraylist of integers
+    public BinaryHeap(ArrayList<Integer> input){
+
+        this.heap = new ArrayList<>(); // create heap
+        this.heap.add(0); // set first idx to 0
+        this.heap.addAll(input); // add numbers
+
+        buildHeap(0); // apply heap-order property
+    }
+
+
+    // build a heap with integers stored in heap ArrayList
+    // left child is 2j, right child is 2j+1
+    public void buildHeap(int idx) {
+        int currentNode = heap.get(idx);
+
+        try {
+            int leftChild = heap.get(2 * idx);
+            int rightChild = heap.get(2 * idx + 1);
+
+
+            if (currentNode > leftChild || currentNode > rightChild) {
+                if (leftChild < rightChild) {
+                    heap.set(idx, leftChild);
+                    heap.set(2 * idx, currentNode);
+                } else {
+                    heap.set(idx, rightChild);
+                    heap.set(2 * idx + 1, currentNode);
+                }
+            }
+            else {
+                idx++;
+            }
+
+
+            buildHeap(idx);
+        }
+        catch(IndexOutOfBoundsException e){}
+    }
+
+    // delete minimum value and return it
+    public int deleteMin(){
+        int output = heap.get(0);
+        heap.set(0, heap.get(heap.size() - 1));
+        heap.remove(heap.size() - 1);
+        buildHeap(0);
+        return output;
+    }
+
+    public ArrayList<Integer> outputAndClear(){
+        ArrayList<Integer> output = new ArrayList<>();
+
+        int outputInt = this.deleteMin();
+
+        try {
+            while (true) {
+                output.add(outputInt);
+                outputInt = this.deleteMin();
+            }
+        }
+        catch (IndexOutOfBoundsException e){
+
+        }
+        return output;
+    }
+}
--- HomeworkOne/src/Main.java
+++ HomeworkOne/src/Main.java
@@ -1,5 +1,40 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("hello world");
+    public static void main(String[] args) throws IOException {
+
+        // input init
+        FileReader input = new FileReader("HomeworkOne/src/input.txt");
+        BufferedReader reader = new BufferedReader(input);
+
+        // read numbers to arraylist
+        String numString = reader.readLine();
+        ArrayList<Integer> unsortedNumbers = new ArrayList<>();
+
+        while(numString != null){
+            Integer num = Integer.parseInt(numString);
+            unsortedNumbers.add(num);
+            numString = reader.readLine();
+        }
+
+        // create binary heap with numbers
+        BinaryHeap binaryHeap = new BinaryHeap(unsortedNumbers);
+
+        // clear list and store in order with deleteMin going to an arrayList for output
+        ArrayList<Integer> outputList = binaryHeap.outputAndClear();
+
+
+        // output init
+        FileWriter output = new FileWriter("/Users/jacob_kitz/IdeaProjects/ECE5330/HomeworkOne/src/output.txt");
+        BufferedWriter writer = new BufferedWriter(output);
+
+        // output
+        for(int number : outputList){
+            writer.write(String.valueOf(number)); // write the number
+            writer.newLine(); // new line
+            writer.flush(); // flush buffer
+        }
+
     }
 }
